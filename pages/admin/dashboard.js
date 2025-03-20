71b5fde0096e8a16e94df4ec242f28839f6dfc38
niranjanbaviskar <157515954+niranjanbaviskar@users.noreply.github.com>
2025-03-20 21:58:52 +0530

    Delete pages directory

--- pages/admin/dashboard.js
+++
@@ -1,9 +0,0 @@
-import AdminDashboardWrapper from "@/components/AdminDashboardWrapper";
-
-export default function Dashboard() {
-    return (
-        <AdminDashboardWrapper>
-            <h1>Hello Admin</h1>
-        </AdminDashboardWrapper>
-    )
-}
